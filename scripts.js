d6b66b07807999b3b4f82c3d0d8e4147bb241fd7
Kyle Melton <87396239+KyleMel@users.noreply.github.com>
2021-11-08 11:29:38 -0600

    updates formatting of stylesheet and html

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const output = document.querySelector('[js-output]')
+const equal = document.querySelector('[ja-equal-button')
+const allclear = document.querySelector('[js-all-clear-button]')
+const numberButton = document.querySelectorAll('[js-number-button')
+
+
+class Calculator {
+    constructor(previousNum, currentNum){
+        
+    }
+
+    displayOutput() {
+
+    }
+    Computation() {
+        switch (number) {
+            case '+':
+                break
+            case '-':
+                break
+            case '*':
+                break
+            case '/':
+                break
+            default:
+                return
+        }
+    }
+}
